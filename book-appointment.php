502a30e9db0d71fec409273ee39c9111adf0903a
rukyaislam <72370986+rukyaislam@users.noreply.github.com>
2021-05-18 10:47:23 +0600

    Add files via upload

---
+++ book-appointment.php
@@ -0,0 +1,290 @@
+<?php
+session_start();
+//error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+
+if(isset($_POST['submit']))
+{
+
+$specilization=$_POST['Doctorspecialization'];
+$doctorid=$_POST['doctor'];
+$userid=$_SESSION['id'];
+//$fees=$_POST['fees'];
+$appdate=$_POST['appdate'];
+$time=$_POST['apptime'];
+$userstatus=1;
+$docstatus=1;
+
+	
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>User  | Book Appointment</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+		<script>
+function getdoctor(val) {
+	$.ajax({
+	type: "POST",
+	url: "get_doctor.php",
+	data:'specilizationid='+val,
+	success: function(data){
+		$("#doctor").html(data);
+	}
+	});
+}
+</script>	
+
+
+<script>
+function getfee(val) {
+	$.ajax({
+	type: "POST",
+	url: "get_doctor.php",
+	data:'doctor='+val,
+	success: function(data){
+		$("#fees").html(data);
+	}
+	});
+}
+</script>	
+
+
+
+
+	</head>
+	<body>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+			<div class="app-content">
+			
+						<?php include('include/header.php');?>
+					
+				<!-- end: TOP NAVBAR -->
+				<div class="main-content" >
+					<div class="wrap-content container" id="container">
+						<!-- start: PAGE TITLE -->
+						<section id="page-title">
+							<div class="row">
+								<div class="col-sm-8">
+									<h1 class="mainTitle">User | Sample Information</h1>
+																	</div>
+								<ol class="breadcrumb">
+									<li>
+										<span>User</span>
+									</li>
+									<li class="active">
+										<span>Sample Information</span>
+									</li>
+								</ol>
+						</section>
+						<!-- end: PAGE TITLE -->
+						<!-- start: BASIC EXAMPLE -->
+						<div class="container-fluid container-fullw bg-white">
+							<div class="row">
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-8 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title"> Sample Information</h5>
+												</div>
+												<div class="panel-body">
+								<p style="color:red;"><?php echo htmlentities($_SESSION['msg1']);?>
+								<?php echo htmlentities($_SESSION['msg1']="");?></p>	
+													<form role="form" name="book" method="post" >
+														
+
+
+<div class="form-group">
+															<label for="DoctorSpecialization">
+																<!-- which type Specialization -->
+															Which test you want
+															
+															</label>
+							<select name="Doctorspecialization" class="form-control" onChange="getdoctor(this.value);" required="required">
+																<option value=""> test name</option>
+																<option>corona 200tk</option>
+																<option> Thyroid 300tk</option>														
+																<option>Hiv 700 tk</option>
+																<option>blood 20tk</option>
+																<option> Diabetics 30tk</option>
+																<option> alergi 300tk</option>
+																<option> vitamin 100tk</option>
+ 
+ 
+ 
+ 
+ 
+
+
+																
+															</select>
+														</div>
+
+
+
+
+														 <div class="form-group">
+															<label for="doctor">
+															 contact number
+															</label>
+						<input type="text" name="doctor" class="form-control" id="doctor"   required="required">
+					 
+
+ 
+
+
+						</input>
+														</div>
+
+ 
+
+														<!-- <div class="form-group">
+															<label for="consultancyfees">
+																Consultancy Fees
+															</label>
+					<select name="fees" class="form-control" id="fees"  readonly>
+						
+						</select>
+														</div> -->
+														 
+<div class="form-group">
+															<label for="AppointmentDate">
+																Date
+															</label>
+									<input class="form-control datepicker" name="appdate"  type="date" required="required">
+														</div>
+														
+<div class="form-group">
+															<label for="Appointmenttime">
+														
+														Time
+													
+															</label>
+									<input class="form-control datepicker" name="apptime" type="time" required="required">
+														</div>														
+														
+														 
+
+
+														<script>
+  function clickAlert() {
+    alert(" Thanks for connect with us . You are successfully put your information .Med-E-test authority will contact very soon");
+}
+</script>
+
+<input type="submit" onclick="clickAlert()" value=" submit">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+														
+													</form>
+												</div>
+											</div>
+										</div>
+											
+											</div>
+										</div>
+									
+									</div>
+								</div>
+							
+						<!-- end: BASIC EXAMPLE -->
+			
+					
+					
+						
+						
+					
+						<!-- end: SELECT BOXES -->
+						
+					</div>
+				</div>
+			</div>
+			<!-- start: FOOTER -->
+	<?php include('include/footer.php');?>
+			<!-- end: FOOTER -->
+		
+			<!-- start: SETTINGS -->
+	<?php include('include/setting.php');?>
+			
+			<!-- end: SETTINGS -->
+		</div>
+		<!-- start: MAIN JAVASCRIPTS -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="vendor/autosize/autosize.min.js"></script>
+		<script src="vendor/selectFx/classie.js"></script>
+		<script src="vendor/selectFx/selectFx.js"></script>
+		<script src="vendor/select2/select2.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- start: CLIP-TWO JAVASCRIPTS -->
+		<script src="assets/js/main.js"></script>
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+		<!-- end: JavaScript Event Handlers for this page -->
+		<!-- end: CLIP-TWO JAVASCRIPTS -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,276 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Med-E-Test</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/responsiveslides.css">
+		<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="js/responsiveslides.min.js"></script>
+		  <script>
+		    // You can also use "$(window).load(function() {"
+			    $(function () {
+
+			      // Slideshow 1
+			      $("#slider1").responsiveSlides({
+			        maxwidth: 1600,
+			        speed: 600
+			      });
+			});
+		  </script>
+	</head>
+	<body>
+		<!--start-wrap-->
+
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.html" style="font-size: 30px;text-decoration: none;">Med-E-Test</a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li class="active"><a href="index.html"style="text-decoration: none;">Home</a></li>
+
+						<li><a href="contact.php"style="text-decoration: none;">contact</a></li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		<div class="clear"> </div>
+			<!--start-image-slider---->
+					<div class="image-slider">
+						<!-- Slideshow 1 -->
+					    <ul class="rslides" id="slider1">
+					      <li><img src="  https://www.ucsfhealth.org/-/media/project/ucsf/ucsf-health/medical-tests/hero/nitroblue-tetrazolium-test-2x.jpg?h=1112&w=2880&hash=88D0D85EAA795553D6B586484B3450A4" alt=""></li>
+					      <li><img src="https://www.expresslabidaho.com/wp-content/uploads/2018/10/test-list.jpg" alt=""></li>
+					      <li><img src="https://www.cdc.gov/coronavirus/2019-ncov/images/lab/cdc-lab-banner.png" alt=""></li>
+					    </ul>
+						 <!-- Slideshow 2 -->
+					</div>
+					<!--End-image-slider---->
+		    <div class="clear"> </div>
+		    <div class="content-grids">
+		    	<div class="wrap">
+		    	<div class="section group">
+
+
+				<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/grid-img3.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Patients</h3>
+						  <!-- <p>Register  </p> -->
+						  <div class="button"><span><a href="hms/user-login.php">Click Here</a></span></div>
+				    </div>
+				</div>
+
+				<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/grid-img1.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h5>pathiologist Login</h5>
+
+						  <div class="button"><span><a href="hms/doctor/">Click Here</a></span></div>
+					</div>
+				</div>
+
+
+					<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/grid-img2.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Admin Login</h3>
+
+						  <div class="button"><span><a href="hms/admin">Click Here</a></span></div>
+				     </div>
+				</div>
+			</div>
+		    </div>
+		   </div>
+		   <div class="wrap">
+		   <div class="content-box">
+		   <div class="section group">
+				<div class="col_1_of_3 span_1_of_3 frist">
+
+				</div>
+				<div class="col_1_of_3 span_1_of_3 second">
+
+				</div>
+				<div class="col_1_of_3 span_1_of_3 frist">
+
+
+				</div>
+			</div>
+		   </div>
+		   </div>
+
+
+
+
+<!-- 
+		   grid -->
+		   <div style="background-color: whitesmoke;">
+		   <div class="container" style="background-color: white;">  
+  <div class="row">
+    <div class="col-6" >
+      
+<div style="background-color:whitesmoke;">
+
+<div style=" color:gray;background-color:white;">
+<br>
+<br><div style="margin-left:30px;margin-top:20px;"><h5>Home Sample Collection Service</h5><br>
+
+Home Sample Collection Service in Dhaka City (COVID & Non-COVID)<br>
+
+Our objective is to deliver high quality special healthcare service to our patients who are unable to come to the hospital to give blood/urine/stool samples for treatment or routine health checkup purpose.
+
+<br><br>Gulshan, Niketon, Banani, Banani DOHS, Baridhara, Baridhara DOHS, Bashundhara, Badda, Mohakhali, Cantonment, Uttara, Abdullahpur, Dhanmondi, Lalmatia, Mohammadpur, Farmgate, Pallabi, Mirpur, Kafrul, Tejgaon, Maghbazar, Banasree, Rampura, Malibagh, Kakrail, Ramna, Arambagh, Motijheel, Khilgaon and Shobujbagh
+
+<br><br>Our trained & skilled phlebotomists are collecting samples from your location.
+
+<br><br>Patient satisfaction is our priority.
+
+<br><br>For COVID/Non-COVID sample collection 0191 400 1301
+
+
+<img src="https://scontent.fzyl2-1.fna.fbcdn.net/v/t1.15752-9/171294037_214789747045582_4246231112638963980_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeEtKX_2rlLoxmZRSnnJOsWRY-bxCIf5lAtj5vEIh_mUC_nXRJoUZfZeHBUxgLSyFJ4hvgJ8LHpqjJVC4RIRCvYh&_nc_ohc=lzyXFYawOOgAX8izY51&_nc_ht=scontent.fzyl2-1.fna&oh=1e905f674b1a51c9e937131bc27e4471&oe=609CA932">
+
+
+</div>
+    </div>
+    </div>
+	</div>
+	<div class="col-6">
+      
+<div>
+<br>
+<br>
+<br>
+
+<div class="card">
+  <div class="card-header" style="background-color: orangered;">
+    Featured
+  </div>
+  <!-- <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> -->
+  <div class="card-header" style="background-color: white;  ">
+   <a href="#" style="text-decoration: none;">services</a>
+  </div>
+  
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> COVID Home Care Package</a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> Chemotherapy </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a hre="#" style="text-decoration: none;">Cafeteria  </a>
+  </div>
+
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;">Corporate Client  </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;">Birth Certificate  </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> 
+Day Care </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;">Death Certificatef  </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;">Blood Bank   </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> ATM </a>
+  </div>
+
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> Appointment Desk   </a>
+  </div>
+  <div class="card-header" style="background-color: white;">
+   <a href="#" style="text-decoration: none;"> Criteria </a>
+  </div>
+
+
+
+
+
+</div>
+
+    </div>
+
+
+
+
+
+</div></div>
+
+  </div>
+		 
+		   
+
+ 
+
+
+
+
+
+
+
+ 
+</div>
+
+		   <div   class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html" style="text-decoration: none;">Home</a></li>
+						<li><a href="contact.php" style="text-decoration: none;">contact</a></li>
+						<li><a href="contact.php"></a></li>
+						<li><a href="contact.php"> </a></li>
+						<li><a href=" "> </a></li>
+						<li>  </li>
+						<li><a href=" "> </a></li><li><a href=" "> </a></li><li><a href=" "> </a></li><li><a href=" "> </a></li><li><a href=" "> </a></li>
+						<li>contact with mail:<a href=" #">mrsas@gmail.com </a></li>
+						<li>contact with phone Number:<a href=" #"> 01740844603 </a></li>
+						<li><a href=" "> </a></li>
+						<li> &copy;all right reserve 2021 MRSAS </li>
+
+					</ul>
+		   	</div>
+
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+
+
+
+
+
+	</body>
+</html>
